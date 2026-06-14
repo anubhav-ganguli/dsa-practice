4548192232b649ef475bb9094e380ec2c5042f03
Anubhav Ganguli <gangulianubhav@yahoo.com>
2026-06-14 21:17:56 +0530

    Add solution: Missing Number

--- solutions/Array/10-missing-number.java
+++ solutions/Array/10-missing-number.java
@@ -1,48 +1,30 @@
 public class Solution {
-    
-    public int MissingNumber(int[] nums) {
-        
-       //return MissingNumberByHashSet(nums);
 
-       return MissingNumberByXOR(nums);
+    //Take a leader and make count as 1. Iterate over elements.If leader same, count++,if leader not same, count--
+    public int MajorityElement(int[] nums) {
+        int leader=nums[0];
 
-    }
+        int count = 1;
 
-
-    public int MissingNumberByHashSet(int[] nums) {
-        
-        HashSet<int> set = new HashSet<int>(nums);
-        for(int i=0;i<=nums.Length;i++)
-        {
-            if(!set.Contains(i))
+         for(int i=1;i<nums.Length;i++)
+         {
+            if(count==0)
             {
-                return i;
+                leader = nums[i];
+                count=1;
             }
-        }
+            else if(nums[i] == leader)  
+              {
+                count++;
+              }
+            else if(nums[i] != leader)  
+              {
+                count--;
+              }
 
-        return -1;
 
-    }
-
-     public int MissingNumberByXOR(int[] nums) {
-        
-        //XOR of any element with itself is 0.
-        //XOR of any element with 0 is the element itself.
-        //XOR of all elemenets from 0 to n , now XOR ing each element from input will cancel the elements and will be left with the missing element
-
-        //XORing all the elements
-        int XOR=0;
-        for(int i=0;i<=nums.Length;i++)
-        {
-            XOR=XOR^i;
-        }
-
-        //to find missing elements from input,xor each element with the xor
-        for(int i=0;i<nums.Length;i++)
-        {
-            XOR=XOR^nums[i];
-        }
-
-        return XOR;
+              
+         }
+        return leader;
     }
 }
