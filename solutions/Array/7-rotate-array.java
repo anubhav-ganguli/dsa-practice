1decd39aead6459ed21ff6b10512fe4b6b1dc981
Anubhav Ganguli <gangulianubhav@yahoo.com>
2026-06-14 21:19:08 +0530

    Add solution: Rotate Array

--- solutions/Array/7-rotate-array.java
+++ solutions/Array/7-rotate-array.java
@@ -1,87 +1,30 @@
 public class Solution {
 
+    //Take a leader and make count as 1. Iterate over elements.If leader same, count++,if leader not same, count--
+    public int MajorityElement(int[] nums) {
+        int leader=nums[0];
 
-//O(n) space complexity
- public void Rotate_WithSpace(int[] nums, int k) {
+        int count = 1;
 
-        //Simplifying k
-        if(k>nums.Length)
-           k = k%nums.Length;
-
-        int[] result= new int[nums.Length];
-
-        int counter=0;
-        //Copying the last part to new array
-        for(int i=nums.Length-k ; i<nums.Length;i++)
-        {
-            result[counter++]=nums[i];
-        }
-
-        //copying the rest to new array
-          for(int i=0 ; i<nums.Length-k;i++)
-        {
-            result[counter++]=nums[i];
-        }
-
-        //Assigning the new array to the input
-          for(int i=0 ; i<nums.Length;i++)
-        {
-            nums[i]=result[i];
-        }
- }
+         for(int i=1;i<nums.Length;i++)
+         {
+            if(count==0)
+            {
+                leader = nums[i];
+                count=1;
+            }
+            else if(nums[i] == leader)  
+              {
+                count++;
+              }
+            else if(nums[i] != leader)  
+              {
+                count--;
+              }
 
 
-    //Better Approach : Reversing the array O(1) Space Complexity
-    //Reversing technique
-    public void Rotate(int[] nums, int k) {
-        if(k > nums.Length)
-            k = k%nums.Length;
-     
-
-     //Reversing entire array
-        int L = 0;
-        int R = nums.Length - 1;
-
-        while (L < R)
-        {
-            int temp = nums[L];
-            nums[L] = nums[R];
-            nums[R] = temp;
-            L++;
-            R--;
-            
-        }
-
-        //Reversing first part
-         L = 0;
-         R =nums.Length ==1? 0: k - 1;
-
-        while (L < R)
-        {
-            int temp = nums[L];
-            nums[L] = nums[R];
-            nums[R] = temp;
-            L++;
-            R--;
-            
-        }
-
-         //Reversing second first part
-         L = k;
-         R = nums.Length - 1;
-
-        while (L < R)
-        {
-            int temp = nums[L];
-            nums[L] = nums[R];
-            nums[R] = temp;
-            L++;
-            R--;
-            
-        }
-
-
-
-
+              
+         }
+        return leader;
     }
 }
