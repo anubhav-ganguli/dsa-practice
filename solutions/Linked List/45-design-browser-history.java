bd9fdb0706a58cd03a918fee1f2c14d37280b23d
Anubhav Ganguli <gangulianubhav@yahoo.com>
2026-07-08 16:54:03 +0530

    Add solution: Design Browser History

---
+++ solutions/Linked List/45-design-browser-history.java
@@ -0,0 +1,61 @@
+public class BrowserHistory {
+
+    Node curr=null;
+    public BrowserHistory(string homepage) {
+        
+       curr = new Node(homepage);
+     
+    }
+    
+    public void Visit(string url) {
+        
+        Node node =  new Node(url);
+        node.prev=curr;
+        curr.next=node;
+        curr=curr.next;
+        
+    }
+    
+    public string Back(int steps) {
+      
+      int i=1;
+      while(curr.prev!=null && i<=steps)
+      {
+        curr=curr.prev;
+        i++;
+      }
+      return curr.data;
+    }
+    
+    public string Forward(int steps) {
+         int i=1;
+      while(curr.next!=null && i<=steps)
+      {
+        curr=curr.next;
+        i++;
+      }
+      return curr.data;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.Visit(url);
+ * string param_2 = obj.Back(steps);
+ * string param_3 = obj.Forward(steps);
+ */
+
+public class Node
+{
+    public string data;
+    public Node next;
+    public Node prev;
+
+    public Node(string data)
+    {
+        this.data = data;
+        next = null;
+        prev = null;
+    }
+}
